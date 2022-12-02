5df25a847336f708c1d84befad404ae23e05743d
Joakim Wassberg <joakim.wassberg@svenskaspel.se>
2022-12-02 09:39:37 +0100

    Updated Dec 02

--- dec02/dec02.py
+++ dec02/dec02.py
@@ -1,51 +1,40 @@
 from utils.data_reader import get_puzzle_input
-def part1(data):
+
+
+def solver(data, part):
     col1 = {"A": 1, "B": 2, "C": 3}
-    col2 = {"X": 1, "Y": 2, "Z": 3}
-        
+    col2 = {"X": 1, "Y": 2, "Z": 3} if part == 1 else {"X": 0, "Y": 3, "Z": 6}
     total = 0
-    
     for game in data:
         player1, player2 = game.split()
         player1 = col1[player1]
         player2 = col2[player2]
 
-        match player2 - player1:
-            case 0:
-                total += 3
-            case -2 | 1:
-                total += 6
-        total += player2
+        if part == 1:
+            match player2 - player1:
+                case 0:
+                    total += 3
+                case -2 | 1:
+                    total += 6
+            total += player2
+        else:
+            match player2:
+                case 6: 
+                    y = player1 % 3 + 1
+                case 3: 
+                    y = player1
+                case 0: 
+                    y = (player1 + 1) % 3 + 1
 
+            total += y + player2
     return total
-    
-def part2(data):
-    col1 = {"A": 1, "B": 2, "C": 3}
-    col2 = {"X": 0, "Y": 3, "Z": 6}
 
-    total_score = 0
-
-    for line in data:
-        player1, player2 = line.split()
-        x = col1[player1]
-        round_score = col2[player2]
-
-        match round_score:
-            case 6:  # win
-                y = x % 3 + 1
-            case 3:  # draw
-                y = x
-            case 0:  # loss
-                y = (x + 1) % 3 + 1
-
-        total_score += y + round_score
-    return total_score
 
 def main():
     data = get_puzzle_input('02a.txt')
 
-    print(f'Puzzle 1: {part1(data)}')
-    print(f'Puzzle 2: {part2(data)}')
+    print(f'Puzzle 1: {solver(data, 1)}')
+    print(f'Puzzle 2: {solver(data, 2)}')
 
 
 if __name__ == '__main__':
